e18f6708b3564968ee579ee89c84b3e3e92c9984
dyngq <dyngqk@163.com>
2021-03-29 19:16:53 +0800

    leetcode-wsl

--- 2019-2022_刷题/JZ剑指offer/59 - II. 队列的最大值.py
+++ 2019-2022_刷题/JZ剑指offer/59 - II. 队列的最大值.py
@@ -1,16 +1,16 @@
 class MaxQueue:
 
     def __init__(self):
-
+        pass
 
     def max_value(self) -> int:
-
+        pass
 
     def push_back(self, value: int) -> None:
-
+        pass
 
     def pop_front(self) -> int:
-
+        pass
 
 
 # Your MaxQueue object will be instantiated and called as such:
